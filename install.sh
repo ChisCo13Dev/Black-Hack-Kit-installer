4b50105404b9b0bbd945e6728903a97da0f64dbe
Erubiel <144752782+ChisCo13Dev@users.noreply.github.com>
2024-05-28 11:48:42 -0600

    Install Black Hack Tools
    este script es para poder instalar la herramienta

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+GREEN='\033[92m'
+CYAN='\033[96m'
+WHITE='\033[97m'
+RED='\033[91m'
+BLUE='\033[94m'
+END='\033[0m'
+
+mostrar_menu() {
+    clear
+    echo -e "${GREEN}Selecciona una opción:"
+    echo -e "${GREEN}1. ${CYAN}Instalar Dependencias"
+    echo -e "${GREEN}2. ${CYAN}Instalar La herramienta"
+    echo -e "${GREEN}00. ${CYAN}Salir"
+}
+
+instalar_dependencias() {
+    echo "${GREEN}Instalando Megatools..."
+    apt install megatools -y  
+    echo -e "${GREEN}Instalando unzip"
+    apt install unzip
+}
+
+instalar_herramienta() {
+    echo -e "${GREEN}Instalando la herramienta espere un poco..."
+    cd ..
+    megadl https://mega.nz/folder/7J5EFaKY#RRc_g5NF6oS3FZKiUFwUFA
+    unzip 'Black Hack Kit.zip'
+    rm -rf 'Black Hack Kit.zip'
+    cd 'Black Hack Kit'
+    python3 index.py
+}
+
+while true; do
+    mostrar_menu
+
+    read -p "Ingresa el número de la opción: " opcion
+
+    case $opcion in
+        1)
+            instalar_dependencias
+            ;;
+        2)
+            instalar_herramienta
+            ;;
+        00)
+            echo "Saliendo..."
+            break
+            ;;
+        *)
+            echo "Opción inválida. Por favor, selecciona una opción válida."
+            ;;
+    esac
+done
